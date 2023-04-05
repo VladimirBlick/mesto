b41a62ad6396d9d64d441bdc0302ca3f10b245f2
VladimirBlick <blick3@yandex.ru>
2023-04-05 23:49:59 +0600

    Popup2

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 const openPopupBtn = document.querySelector('.profileInfo__popUp_open');
 const popup = document.querySelector('.popup');
+const popupCloseBtn = document.querySelector('.popup__close-btn');
 
 function togglePopup () {
-  popup.classList.toggle('popup_open')
+  popup.classList.toggle('popup_open');
 }
 
-openPopupBtn.addEventListener('click', togglePopup)
+openPopupBtn.addEventListener('click', togglePopup);
+
+popupCloseBtn.addEventListener('click', togglePopup);
