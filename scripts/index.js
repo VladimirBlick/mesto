fd81414907f10f69c629b297f4724ae95366f260
Vladimir BLick <blick3@yandex.ru>
2023-04-10 17:45:45 +0600

    popup9apr

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
+//открытие и закрытие поп-ап
 const openPopupBtn = document.querySelector('.profileInfo__popUp_open');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.popup__close-btn');
+const popupBtn = document.querySelector('.popup__btn')
 
 function togglePopup () {
   popup.classList.toggle('popup_open');
@@ -9,3 +11,35 @@ function togglePopup () {
 openPopupBtn.addEventListener('click', togglePopup);
 
 popupCloseBtn.addEventListener('click', togglePopup);
+
+
+//запись с инпута на страницу
+const formPopup = document.querySelector('.popup__btn').addEventListener('click', myClick);
+
+function myClick(){
+const changeName = document.querySelector('.popup__input_name').value;
+ document.querySelector('.profileInfo__name').innerHTML = changeName;
+ const changeJob = document.querySelector('.popup__input_job').value;
+ document.querySelector('.profileInfo__job').innerHTML = changeJob;
+}
+
+function closePopup(){
+
+  popupCloseBtn = document.querySelector('.popup__close-btn');
+
+}
+
+let formElement = document.querySelector('.popup__form')
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__input_name')
+let jobInput = document.querySelector('.popup__input_job')
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
