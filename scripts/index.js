c767ad0364705d80a346db4b5bb9047f4029bdb3
Vladimir BLick <blick3@yandex.ru>
2023-04-12 18:12:15 +0600

    first iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,40 @@
-const openPopupBtn = document.querySelector('.profileInfo__popUp_open');
+const openPopupBtn = document.querySelector('.profile__popup_opened');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.popup__close-btn');
 const popupBtn = document.querySelector('.popup__btn');
 
 function togglePopup() {
-  popup.classList.toggle('popup_open');
+  popup.classList.toggle('popup_opened');
+  const profileName = document.querySelector('.profile__name');
+  const inputName = document.querySelector('.popup__input_type_name');
+  inputName.value = profileName.textContent;
 }
 
 openPopupBtn.addEventListener('click', togglePopup);
 popupCloseBtn.addEventListener('click', togglePopup);
 
-document.querySelector('.popup__btn').addEventListener('click', myClick);
+const submitBtn = document.querySelector('.popup__btn').addEventListener('click', handleFormSubmit);
 
-function myClick() {
-  const changeName = document.querySelector('.popup__input-name').value;
-  document.querySelector('.profileInfo__name').innerHTML = changeName;
-  const changeJob = document.querySelector('.popup__input-job').value;
-  document.querySelector('.profileInfo__job').innerHTML = changeJob;
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  const changeName = document.querySelector('.popup__input_type_name').value;
+  document.querySelector('.profile__name').innerHTML = changeName;
+  const changeJob = document.querySelector('.popup__input_type_job').value;
+  document.querySelector('.profile__job').innerHTML = changeJob;
+  togglePopup();
 }
 
-popupBtn.addEventListener('click', togglePopup);
 
-const profileName = document.querySelector('.profileInfo__name');
-const inputName = document.querySelector('.popup__input-name');
-inputName.value = profileName.textContent;
 
-const profileJob = document.querySelector('.profileInfo__job');
-const inputJob = document.querySelector('.popup__input-job');
+const profileJob = document.querySelector('.profile__job');
+const inputJob = document.querySelector('.popup__input_type_job');
 inputJob.value = profileJob.textContent;
 
 const formElement = document.querySelector('.popup__form');
 
-document.querySelector('.popup__input-name');
-document.querySelector('.popup__input-job');
+document.querySelector('.popup__input_type_name');
+document.querySelector('.popup__input_type_job');
 
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-}
+
 
 formElement.addEventListener('submit', handleFormSubmit);
