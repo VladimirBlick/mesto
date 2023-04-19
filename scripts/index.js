f81cdeb35fac62195d06c35f08ffe9a68cd4c2c6
Vladimir BLick <blick3@yandex.ru>
2023-04-19 18:00:01 +0600

    PR5

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+//Попап для изменения имени и работы (практическая №4)
+//переменные для пр4
 const popup = document.querySelector('.popup');
 const popupContainer = document.querySelector('.popup__container')
 const openPopupBtn = document.querySelector('.profile__popup');
@@ -8,6 +10,10 @@ const profileJob = document.querySelector('.profile__job');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputJob = document.querySelector('.popup__input_type_job');
 
+//переменные для пр5
+const cityLike = document.querySelector('element__city-like');
+
+//функции для пр4
 function openPopup() {
   popup.classList.add('popup_opened');
   inputJob.value = profileJob.textContent;
@@ -25,6 +31,18 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 
+//слушатели для пр4
 openPopupBtn.addEventListener('click', openPopup);
 closePopupeBtn.addEventListener('click', closePopup);
 popupContainer.addEventListener('submit', handleFormSubmit);
+
+//Шесть карточек «из коробки»
+//Форма добавления карточки
+//Добавление карточки
+
+//Лайк карточки
+
+
+
+//Удаление карточки
+//Открытие попапа с картинкой
