9116267faf1e9666c3c6db78e8f733e3cf19b57c
Vladimir BLick <blick3@yandex.ru>
2023-04-21 17:26:20 +0600

    JS cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 //попап имя, работа
 const popup = document.querySelector('.popup');
 const popupContainer = document.querySelector('.popup__container');
@@ -20,6 +21,67 @@ const imageLink = document.querySelector('.profile__job');
 //лайк карточки
 const cardLike = document.querySelector('.element__city-like');
 
+//Шесть карточек «из коробки» и добавление новых
+const initialCards = [
+  {
+    name: 'Омск',
+    link: 'https://images.unsplash.com/photo-1653828030585-1aa093033717'
+  },
+  {
+    name: 'Новосибирск',
+    link: 'https://images.unsplash.com/photo-1564502045820-95ee6fb9d7de'
+  },
+  {
+    name: 'Москва',
+    link: 'https://images.unsplash.com/photo-1513326738677-b964603b136d'
+  },
+  {
+    name: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1556610961-2fecc5927173'
+  },
+  {
+    name: 'Калининград',
+    link: 'https://images.unsplash.com/photo-1621707098150-3c0b7de2c3ef'
+  },
+  {
+    name: 'Рязань',
+    link: 'https://images.unsplash.com/photo-1664277132722-b6340560eda9'
+  }
+];
+
+/*const cardTemplate = document.getElementById('card-template');
+const elements = document.querySelector('.elements');
+
+const createCard = function cardData (){
+ const cardElements = cardTemplate.content.querySelector('.element').cloneNode(true);
+ const elementImage = cardElements.querySelector('.element__image');
+ const elementCityName = cardElements.querySelector('.element__city-name');
+ elementImage.src = initialCards.link
+ elementCityName.textContent = initialCards.name
+
+ return cardElements;
+}
+
+initialCards.forEach(function(element)) =*/
+
+  // Получаем элементы из DOM
+  const templateElement = document.getElementById('card-template');
+  const elementsContainer = document.querySelector('.elements');
+
+  initialCards.forEach(function(card) {
+    // Создаем новый элемент на основе шаблона
+    const element = templateElement.content.cloneNode(true).querySelector('.element');
+
+console.log(card.link);
+    // Заполняем элемент данными из массива ·initialCards`
+    element.querySelector('.element__image').src = card.link;
+    element.querySelector('.element__image').alt = card.name;
+    element.querySelector('.element__city-name').textContent = card.name;
+
+    // Добавляем элемент внутрь контейнера `elementsContainer`
+    elementsContainer.appendChild(element);
+  });
+
 //функции попап имя, работа
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -40,8 +102,9 @@ function handleFormSubmit(evt) {
 
 //функция лайк карточки
 function like () {
-  cardLike.classList.toggle('element__city-like_active')
+  cardLike.classList.toggle('element__city-like_active');
 }
+
 
 //функции попап карточка
 function openPopupCard() {
@@ -62,7 +125,7 @@ function handleFormCreate(evt) {
 }
 
 //слушатель лайк карточки
-cardLike.addEventListener('click', like);
+//cardLike.addEventListener('click', like);
 
 //слушатели для попап имя, работа
 openPopupBtn.addEventListener('click', openPopup);
@@ -76,7 +139,7 @@ popupCardContainer.addEventListener('submit', handleFormCreate);
 
 
 
-//Шесть карточек «из коробки»
+
 //Форма добавления карточки
 //Добавление карточки
 
